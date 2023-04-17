6e947d8959202c8db4350ae7bc51de34934d5e23
RichardOrlandini <richardorlandini09@gmail.com>
2023-04-17 18:13:36 -0300

    first commit

---
+++ src/test/java/com/richardorlandini/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richardorlandini.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
