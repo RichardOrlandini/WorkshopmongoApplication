dc3a4ff130d16139c5d57a0b6d2e87c721e85ec4
RichardOrlandini <richardorlandini09@gmail.com>
2023-04-18 15:06:52 -0300

    Obtendo um post por id

---
+++ src/main/java/com/richardorlandini/workshopmongo/services/PostService.java
@@ -0,0 +1,21 @@
+package com.richardorlandini.workshopmongo.services;
+
+import com.richardorlandini.workshopmongo.domain.Post;
+import com.richardorlandini.workshopmongo.repository.PostRepository;
+import com.richardorlandini.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+
+public class PostService {
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
