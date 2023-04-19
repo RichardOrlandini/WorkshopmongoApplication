0ceb4451589d2bb60fe82671aff77a6da39e1544
RichardOrlandini <richardorlandini09@gmail.com>
2023-04-19 11:04:41 -0300

    Consulta simples com @query

--- src/main/java/com/richardorlandini/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/richardorlandini/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.richardorlandini.workshopmongo.repository;
 import com.richardorlandini.workshopmongo.domain.Post;
 import com.richardorlandini.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,11 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }") // o regex e o valor 0, e o 1 parametro do metodo abaixo
+    List<Post> searchTitle(String text);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
     // buscando o tile com like e ignorando letras maisculas e minusculas.
+    // Spriong data gera a consulta automaticamente.
+
 }
